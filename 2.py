ec8b17e01164b9f7aea116a4ddc35f094e93a44d
iwantww123 <95857590+iwantww123@users.noreply.github.com>
2021-12-23 21:10:56 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,31 @@
+#1A2B game
+import streamlit as st
+import random
+
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+answer=random.shuffle(items)
+answer=''
+a_count=0 # initial A count
+b_count=0 # initial B count
+
+for i in range(4):
+    answer+=str(items[i])
+while(True):
+    number=st.text_input('請輸入數字： ')
+    st.write(answer)
+    if not number.isdigit():  #cheak all input is digit
+        pass
+    else:
+        if number==answer:
+            st.write('很棒你答對了')
+            break
+        for i in range(4):
+            for j in range(4):
+                if i==j and number[i]==answer[j]:
+                    a_count+=1
+                elif number[i]==answer[j]:
+                    b_count+=1
+        st.write('{0}A{1}B'.format(a_count,b_count))
+        a_count=0
+        b_count=0
+        
